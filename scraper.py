6c17b96985c0b880fc5ed05216c6cde6bad577a2
Eason12030105 <eason12030105@gmail.com>
2022-12-25 15:27:43 +0800

    5.4 動態網頁彈跳視窗關閉方法

--- scraper.py
+++ scraper.py
@@ -1,21 +1,28 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.common.action_chains import ActionChains
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get('https://shopee.tw/mall/%E5%B1%85%E5%AE%B6%E7%94%9F%E6%B4%BB-cat.11040925')
 
 time.sleep(5)
 
-cards = driver.find_element(By.CSS_SELECTOR, "div[class='UpbuCz']")
+ActionChains(driver).move_by_offset(100,100).click().perform()
 
-Items = []
-for card in cards:
-    title = card.find_element(By.CSS_SELECTOR, "div[class='S-VCpK JFJ4OD _88FgR3']").text
-    price = card.find_element(By.CSS_SELECTOR, "div[class='A9okRS']").text
-    link = card.find_element(By.TAG_NAME, "a").get_sttribute('href')
+#cards = driver.find_elements(By.CSS_SELECTOR, "div[class='UpbuCz']")
 
-    Items.append((title,price,link))
+cards = driver.find_elements(By.TAG_NAME, "li")
+print(cards)
 
-print(Items)
+# items = []
+# for card in cards:
+#     title = card.find_elements(By.CSS_SELECTOR, "div[class='S-VCpK JFJ4OD _88FgR3']").text
+#     price = card.find_elements(By.CSS_SELECTOR, "div[class='A9okRS']").text
+#     link = card.find_elements(By.TAG_NAME, "a").get_sttribute('href')
+
+#     items.append((title,price,link))
+
+# print(items)
---
+++ scraper1.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+driver = webdriver.Chrome(executable_path='./chromedriver')
+driver.implicitly_wait(15)
+driver.get(url='https://shopee.tw/mall/%E5%B1%85%E5%AE%B6%E7%94%9F%E6%B4%BB-cat.11040925')
+
+html_source=driver.page_source
+driver.quit
+
+soup =BeautifulSoup(html_source, 'lxml')
+
+table = soup.select('table')[0]
+
+print(table)
