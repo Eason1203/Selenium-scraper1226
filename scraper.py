5153623629fb12d23379075746b9b7cb8e9cd457
Eason12030105 <eason12030105@gmail.com>
2022-12-25 15:41:35 +0800

    5.5 自動化移動滑鼠載入動態資料

--- scraper.py
+++ scraper.py
@@ -3,26 +3,27 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.action_chains import ActionChains #關閉彈跳視窗
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get('https://shopee.tw/mall/%E5%B1%85%E5%AE%B6%E7%94%9F%E6%B4%BB-cat.11040925')
 
 time.sleep(5)
 
-ActionChains(driver).move_by_offset(100,100).click().perform()
+ActionChains(driver).move_by_offset(100,100).click().perform() #關閉彈跳視窗
 
 #cards = driver.find_elements(By.CSS_SELECTOR, "div[class='UpbuCz']")
 
-cards = driver.find_elements(By.TAG_NAME, "li")
+cards = driver.find_elements(By.TAG_NAME, "div")
 print(cards)
 
-# items = []
-# for card in cards:
-#     title = card.find_elements(By.CSS_SELECTOR, "div[class='S-VCpK JFJ4OD _88FgR3']").text
-#     price = card.find_elements(By.CSS_SELECTOR, "div[class='A9okRS']").text
-#     link = card.find_elements(By.TAG_NAME, "a").get_sttribute('href')
+items = []
+for card in cards:
+    ActionChains(driver).move_to_element(card).perform()
+    title = card.find_elements(By.CSS_SELECTOR, "div[class='S-VCpK JFJ4OD _88FgR3']").text
+    price = card.find_elements(By.CSS_SELECTOR, "div[class='A9okRS']").text
+    link = card.find_elements(By.TAG_NAME, "a").get_sttribute('href')
 
-#     items.append((title,price,link))
+    items.append((title,price,link))
 
-# print(items)
+print(items)
