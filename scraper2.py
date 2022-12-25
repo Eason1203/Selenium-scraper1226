ac18ea1f240085fbace45c94482543f87ac0248c
Eason12030105 <eason12030105@gmail.com>
2022-12-25 17:35:10 +0800

    6.5 自動化滾動捲軸載入動態資料

--- scraper2.py
+++ scraper2.py
@@ -3,18 +3,28 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 import credentials
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--disable-notifications")
 
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
 driver.get('https://www.facebook.com/')
 
 email = driver.find_element(By.ID, "email")
 password = driver.find_element(By.ID, "pass")
 
 email.send_keys(credentials.email)
-time.sleep(5)
+time.sleep(2)
 password.send_keys(credentials.password)
-time.sleep(5)
+time.sleep(2)
 password.submit()
 
-time.sleep(60)
+time.sleep(5)
+
+driver.get("https://www.facebook.com/ETtodayMOVIE")
+
+for i in range(20):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+    time.sleep(3)
